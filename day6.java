39684e8be2cbdb16d0c544180fe449bacd2f7355
Muhammad Ismayeel <ismayeelismu80@gmail.com>
2025-10-16 20:56:23 +0530

    HTTP, APIs, and Spring Boot Introduction

---
+++ day6.java
@@ -0,0 +1,12 @@
+package com.example.day6;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello, Java Backend!";
+    }
+}
